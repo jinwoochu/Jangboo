5145545fa8ca6fabcd6aefc58a6a0f6003bbd820
jinwoochu <cjw0672@naver.com>
2018-04-30 00:49:19 +0900

    add userRegister

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-var app = express();
-
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+// 로그인 페이지
 router.get('/login', function(req, res, next) {
    res.render("usersLogin");
 });
+
+// 로그인 밸리데이션
+router.post("/login",function (req,res,next) {
+    // var id = req.body.id;
+    // var password = req.body.password;
+
+    // db에 접근해서 로그인 밸리데이션을 한다.
+    
+})
+
+// 회원가입 페이지
+router.get('/register', function (req,res) {
+    res.render('userRegister');
+})
+
+// 회원가입 밸리데이션
+
+
 
 
 

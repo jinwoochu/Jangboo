b895ab88f3e4e0367bb2e209555ed5a328b7e3d1
jinwoochu <cjw0672@naver.com>
2018-06-16 19:57:23 +0900

    update admin cookie function && WithdrawSearch

--- db/admin/lib/adminDB.js
+++ db/admin/lib/adminDB.js
@@ -73,7 +73,7 @@ exports.login = function (req,res) {
             if (result[0].password === password) {
 
                 // 여기서 쿠키 심어야 할듯
-                // res.cookie('id', id, { signed: true });
+                res.cookie('aid', id, { signed: true });
 
                 response = makeResponse(1, "로그인 성공", {});
                 res.json(response);
@@ -89,8 +89,8 @@ exports.login = function (req,res) {
 
 // 로그인되어있는지 , 쿠키 확인
 exports.isLogined = function(req, res, next) {
-    if (req.signedCookies.id === undefined) {
-        res.redirect('/');
+    if (req.signedCookies.aid === undefined) {
+        res.redirect('/admins/login');
     } else {
         next();
     }
